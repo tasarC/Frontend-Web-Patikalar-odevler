acbb9cb4a94cbc57c1b0a739e865826d4d250b87
Cennet Tasar <81915186+tasarC@users.noreply.github.com>
2023-03-28 00:31:42 +0300

    Update index.js

--- React-Odevler/react-odev1/src/index.js
+++ React-Odevler/react-odev1/src/index.js
@@ -1,6 +1,7 @@
 import axios from "axios";
-
+ //'number' olarak tanımlanan bir parametre ile bir asenkron fonksiyon oluşturuldu.
 export default async(number)=>{
+   //kullanıcı ve post verileri bir kimlik kullanarak axios fonksiyonu ile çağrıldı.
    try {
     const {data:users} =await axios(`https://jsonplaceholder.typicode.com/users/${number}`)
     const {data:post} = await axios(`https://jsonplaceholder.typicode.com/posts?userId=${number}`)
