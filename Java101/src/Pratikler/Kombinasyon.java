f80442204f977a73b25842458e749466632e253b
Cennet Tasar <81915186+tasarC@users.noreply.github.com>
2023-06-14 19:15:16 +0300

    Update Kombinasyon.java

--- Java101/src/Pratikler/Kombinasyon.java
+++ Java101/src/Pratikler/Kombinasyon.java
@@ -1,32 +1,42 @@
 package Pratikler;
 import java.util.Scanner;
+
 public class Kombinasyon {
     public static void main(String[] args){
-        int n,r,i,a,nf=1,rf=1,af=1,c;
+        int n, r, i, a, nf = 1, rf = 1, af = 1, c;
         Scanner input = new Scanner(System.in);
+        
+        // Kümenin eleman sayısını kullanıcıdan al
         System.out.print("Küme kaç elemanlı olsun: ");
         n = input.nextInt();
+        
+        // Farklı grupların eleman sayısını kullanıcıdan al
         System.out.print("Kaç elemanlı farklı grupları olsun: ");
         r = input.nextInt();
 
-        // n sayısının faktöriyelini bulmak için:
-        for(i= n; i>0; i--){
+        // n faktöryelini hesapla
+        for(i = n; i > 0; i--){
             nf = nf * i;
         }
 
-        // r sayısının faktöriyelini bulmak için:
-        for(i=r; i>0; i--){
+        // r faktöryelini hesapla
+        for(i = r; i > 0; i--){
             rf = rf * i;
         }
 
-        a = n-r;
+        // a değerini hesapla (n - r)
+        a = n - r;
 
-        // a sayısının faktöriyelini bulmak için:
-        for(i=a; i>0; i--){
+        // a faktöryelini hesapla
+        for(i = a; i > 0; i--){
             af = af * i;
         }
 
+        // Kombinasyon formülüne göre hesapla: c = n! / (r! * (n-r)!)
         c = nf / (rf * af);
+        
+        // Sonucu ekrana yazdır
         System.out.print(c);
     }
 }
+
